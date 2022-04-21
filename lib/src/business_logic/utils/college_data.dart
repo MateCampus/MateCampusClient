6d5191343b759c51168db256c6fab3652c940571
S-YUN <candy9won@naver.com>
2022-04-22 00:55:34 +0900

    func: mypage뷰모델 구현

--- lib/src/business_logic/utils/college_data.dart
+++ lib/src/business_logic/utils/college_data.dart
@@ -17,12 +17,12 @@ class CollegeData {
   // icon를 학교 이미지로 변경 필요, 0000은 빈 값
   static final Map<String, Map<String, String>> data = {
     'college0000': {'name': '', 'image': '\u{1F601}'},
-    'college0001': {'name': '단국대', 'image': '\u{1F601}'},
-    'college0002': {'name': '건국대', 'image': '\u{1F1F7}'},
-    'college0003': {'name': '동국대', 'image': '\u{1F4B5}'},
-    'college0004': {'name': '홍익대', 'image': '\u{2708}'},
-    'college0005': {'name': '중앙대', 'image': '\u{2600}'},
-    'college0006': {'name': '경희대', 'image': '\u{1F308}'},
-    'college0007': {'name': '한국외대', 'image': '\u{1F3AE}'},
+    'college0001': {'name': '단국대학교', 'image': '\u{1F601}'},
+    'college0002': {'name': '건국대학교', 'image': '\u{1F1F7}'},
+    'college0003': {'name': '동국대학교', 'image': '\u{1F4B5}'},
+    'college0004': {'name': '홍익대학교', 'image': '\u{2708}'},
+    'college0005': {'name': '중앙대학교', 'image': '\u{2600}'},
+    'college0006': {'name': '경희대학교', 'image': '\u{1F308}'},
+    'college0007': {'name': '한국외대학교', 'image': '\u{1F3AE}'},
   };
 }
