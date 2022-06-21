728b73c300d9174e5ae092a249b702139eb74447
S-YUN <candy9won@naver.com>
2022-06-21 14:35:45 +0900

    fix: chat_main 상단 광고 배너 auto slide로 수정

--- lib/src/ui/views/chat/chat_main/components/chat_top_banner.dart
+++ lib/src/ui/views/chat/chat_main/components/chat_top_banner.dart
@@ -1,24 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:zamongcampus/src/business_logic/utils/constants.dart';
 import 'package:zamongcampus/src/config/size_config.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class ChatTopBanner extends StatelessWidget {
   const ChatTopBanner({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final _bannerList = [
+      Image.asset('assets/images/temp/banner1.jpg', fit: BoxFit.cover),
+      Image.asset('assets/images/temp/banner2.jpg', fit: BoxFit.cover),
+      Image.asset('assets/images/temp/banner3.jpg', fit: BoxFit.cover),
+    ];
+
     return Padding(
-      padding: EdgeInsets.symmetric(
-          horizontal: getProportionateScreenWidth(20),
-          vertical: getProportionateScreenHeight(10)),
-      child: Container(
-        height: getProportionateScreenHeight(80),
-        width: getProportionateScreenWidth(335),
-        child: Image.asset(
-          'assets/images/temp/zamongad.png',
-          fit: BoxFit.cover,
-        ),
-      ),
-    );
+        padding: EdgeInsets.symmetric(
+            horizontal: getProportionateScreenWidth(20),
+            vertical: getProportionateScreenHeight(10)),
+        child: CarouselSlider(
+          options: CarouselOptions(
+              height: getProportionateScreenHeight(80),
+              viewportFraction: 1.0,
+              autoPlay: true,
+              autoPlayCurve: Curves.easeInOut,
+              autoPlayInterval: const Duration(seconds: 5)),
+          items: _bannerList.map((bannerImg) {
+            return Builder(builder: (context) {
+              return ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: bannerImg,
+              );
+            });
+          }).toList(),
+        ));
   }
 }
