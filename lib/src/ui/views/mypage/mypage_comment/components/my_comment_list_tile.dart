e7dc5f6865320086d309c990ff2770022fe4e37f
S-YUN <candy9won@naver.com>
2022-06-28 10:38:42 +0900

    ui: 내 댓글 페이지 ui 작업 및 삭제 기능 구현

--- lib/src/ui/views/mypage/mypage_comment/components/my_comment_list_tile.dart
+++ lib/src/ui/views/mypage/mypage_comment/components/my_comment_list_tile.dart
@@ -1,60 +1,70 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:zamongcampus/src/business_logic/constants/color_constants.dart';
 import 'package:zamongcampus/src/business_logic/constants/font_constants.dart';
+import 'package:zamongcampus/src/business_logic/utils/methods.dart';
+import 'package:zamongcampus/src/business_logic/view_models/mypage_comment_viewmodel.dart';
 import 'package:zamongcampus/src/business_logic/view_models/post_detail_screen_viewmodel.dart';
 import 'package:zamongcampus/src/config/size_config.dart';
-import 'package:zamongcampus/src/ui/common_widgets/horizontalDividerCustom.dart';
+import 'package:zamongcampus/src/ui/common_components/custom_alert_dialog_components/delete/commemt_deleted_msg.dart';
 import 'package:zamongcampus/src/ui/common_widgets/horizontal_spacing.dart';
+import 'package:zamongcampus/src/ui/common_widgets/vertical_spacing.dart';
 
 class MyCommentListTile extends StatelessWidget {
+  final MypageCommentViewModel vm;
   final CommentPresentation comment;
-  const MyCommentListTile({Key? key, required this.comment}) : super(key: key);
+  const MyCommentListTile({Key? key, required this.vm, required this.comment})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-            padding: EdgeInsets.symmetric(
-                horizontal: getProportionateScreenWidth(20),
-                vertical: getProportionateScreenHeight(15)),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Text(
-                      comment.userNickname,
-                      style: TextStyle(
-                          color: kMainColor,
-                          fontSize: getProportionateScreenHeight(13),
-                          fontWeight: FontWeight.bold),
-                    ),
-                    const HorizontalSpacing(of: 8),
-                    Text(
-                      comment.createdAt,
-                      style: TextStyle(
-                          fontSize: kCreateAtFontSize,
-                          color: kPostBtnColor,
-                          fontWeight: FontWeight.w300),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                      vertical: getProportionateScreenHeight(7)),
-                  child: Text(
-                    comment.body,
-                    style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: getProportionateScreenHeight(13),
-                        height: 1.3),
-                  ),
-                ),
-              ],
-            )),
-        const HorizontalDividerCustom(color: kMainScreenBackgroundColor)
-      ],
+    return Padding(
+      padding: EdgeInsets.symmetric(
+          horizontal: getProportionateScreenWidth(20),
+          vertical: getProportionateScreenHeight(15)),
+      child: Row(
+        children: [
+          Expanded(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                comment.createdAt,
+                style: TextStyle(
+                    fontSize: kCreateAtFontSize,
+                    color: kPostBtnColor,
+                    fontWeight: FontWeight.w300),
+              ),
+              const VerticalSpacing(of: 5),
+              Text(
+                comment.body,
+                style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: resizeFont(13.5),
+                    color: Colors.black.withOpacity(0.8),
+                    height: 1.5),
+              )
+            ],
+          )),
+          const HorizontalSpacing(of: 10),
+          IconButton(
+            onPressed: () {
+              buildCustomAlertDialog(
+                  context: context,
+                  contentWidget: const CommentDeletedMsg(),
+                  btnText: '삭제',
+                  press: () {
+                    vm.deleteMyComment(context, comment);
+                  });
+            },
+            padding: EdgeInsets.zero,
+            constraints: const BoxConstraints(),
+            icon: const Icon(CupertinoIcons.trash),
+            iconSize: getProportionateScreenWidth(16),
+            color: kMainColor,
+          ),
+        ],
+      ),
     );
   }
 }
