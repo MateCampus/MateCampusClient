7426d60ff8234a56b0bb7aa04d590738ca094878
S-YUN <https://github.com/S-YUN>
2023-02-26 15:31:16 +0900

    func: 차단했는데 구독이 안끊겨서 로컬디비에 저장되어있는 차단한 로그인 아이디를 대조하여 메세지를 ui에 추가하지 않도록 수정

--- lib/src/object/stomp_object.dart
+++ lib/src/object/stomp_object.dart
@@ -61,13 +61,13 @@ class StompObject {
     if (AuthService.loginId != null && AuthService.token != null) {
       /// (23.02.22) 일단, loadchatRooms 자체를 두번해야하는것같다. 이유는 모르겠음. 새롭게 받아온 메세지들 때문인거 같은데. 리팩필요
       /// 아무튼 authService 에서 한번 불러온 상태이고
-      /// 1. fcm 토큰 구독 
+      /// 1. fcm 토큰 구독
       /// 2.밀린 메세지 불러와서 로컬 디비에 세팅
       /// 3. 다시 로컬 디비에서 chatRooms들 불러옴.
       /// 4. 그리고 그 불러온 chatRoom들 모두 구독
       /// 5. 안읽은 메세지 갯수 체크
-     
-     //1
+
+      //1
       subscribeChatRoom(FirebaseObject.deviceFcmToken);
 
       ChatViewModel chatViewModel = serviceLocator<ChatViewModel>();
@@ -78,7 +78,7 @@ class StompObject {
       //4
       for (ChatRoom chatRoom in chatViewModel.chatRooms) {
         chatRoom.unsubscribeFn = subscribeChatRoom(chatRoom.roomId);
-        print(chatRoom.roomId+'번 방 구독하는중');
+        print(chatRoom.roomId + '번 방 구독하는중');
       }
       //5
       await chatViewModel.getTotalUnreadCount();
@@ -113,8 +113,7 @@ class StompObject {
             } else {
               //혹시 chatRoom 삽입(=stomp 구독)이 느렸을경우엔, chatMain 페이지로 넘겨버림
               print('chatmain에서 chatRoom 못찾아서 chatdetail로 못가고 chatmain 으로 왔당');
-              NavigationService()
-                  .pushNamedAndRemoveUntilWithoutArgs("/", "/");
+              NavigationService().pushNamedAndRemoveUntilWithoutArgs("/", "/");
             }
             break;
           case "/voiceDetail":
@@ -131,7 +130,6 @@ class StompObject {
             break;
         }
       }
-      
     }
   }
 
@@ -158,84 +156,93 @@ class StompObject {
 
         if (res["type"] == "talk") {
           /***** TALK ******/
-          ChatMessage chatMessage =
-              ChatMessage.fromJsonRoomId(res["messageDto"], res["roomId"]);
-          // 1. 메세지 저장
-          _chatService.insertMessage(chatMessage);
-          // 2. local db의 chatroom 내용 변경 => 이미 그 방 안이면 0, 아니면 1 추가
-          _chatService.updateChatRoom(
-              lastMsg: chatMessage.text,
-              lastMsgCreatedAt: chatMessage.createdAt,
-              unreadCount:
-                  chatMessage.roomId == chatViewModel.insideRoomId ? 0 : 1,
-              roomId: chatMessage.roomId);
+          bool isBlocked = await PrefsObject.getBlockedUserByLoginId(
+              res["messageDto"]["loginId"]);
+          if (isBlocked) {
+            print('차단했는데 구독이 안끊겨서 스톰으로 메세지를 받아왔지만 화면에 안띄워줌');
 
-          // 3. pref에 최근 메시지 시간 저장할 것
-          PrefsObject.setTotalLastMsgCreatedAt(
-              chatMessage.createdAt.toIso8601String());
-
-          // UI: 현재 위치가 해당방이면 ui 수정(chatdetail)
-          if (chatMessage.roomId == chatViewModel.insideRoomId) {
-            if (chatViewModel.fromFriendProfile) {
-              chatDetailFromFriendProfileViewModel.addChatMessage(chatMessage);
-            } else {
-              chatDetailViewModel.addChatMessage(chatMessage);
-            }
-          }
-          // ** UI(chatsScreen) 변경
-          int index = chatViewModel.getExistRoomIndex(chatMessage.roomId!);
-          // ChatRoom chatRoom = chatViewModel.chatRooms[index];
-          if (index == 0) {
-            // 최상단 : 업데이트만 => lastmsg, unreadcount, lastMsgCreatedAt
-            ChatRoom chatRoom = chatViewModel.chatRooms[index];
-            chatRoom.lastMessage = chatMessage.text;
-            chatRoom.lastMsgCreatedAt = chatMessage.createdAt;
-            // 해당 방 안에 아닐 때만 unreadcount 변경
-            if (chatMessage.roomId != chatViewModel.insideRoomId) {
-              chatRoom.unreadCount = chatRoom.unreadCount + 1;
-            }
-            chatViewModel.replaceItem(chatRoom, index);
+            return;
           } else {
-            // 최상단 아니면 => 1. 기존 방: 방 지웠다가 새롭게 방 추가  2. 완전 처음 방: 새롭게 방만 추가
+            ChatMessage chatMessage =
+                ChatMessage.fromJsonRoomId(res["messageDto"], res["roomId"]);
+            // 1. 메세지 저장
+            _chatService.insertMessage(chatMessage);
+            // 2. local db의 chatroom 내용 변경 => 이미 그 방 안이면 0, 아니면 1 추가
+            _chatService.updateChatRoom(
+                lastMsg: chatMessage.text,
+                lastMsgCreatedAt: chatMessage.createdAt,
+                unreadCount:
+                    chatMessage.roomId == chatViewModel.insideRoomId ? 0 : 1,
+                roomId: chatMessage.roomId);
 
-            if (index >= 0) {
-              // 기존에 있는 방: unreadCount, lastMsg, lastMsgCreatedAt 변경
-              // 현재 그 방 안이라면 0으로 값 변경
-              // 아니면 삭제(removeItem)하면서 unreadcount 반환 (그 값으로 변경)
-              ChatRoom chatRoom = chatViewModel.chatRooms[index];
-              int unreadCount =
-                  chatViewModel.removeItem(index, chatRoom.roomId);
-              if (chatMessage.roomId == chatViewModel.insideRoomId) {
-                chatRoom.unreadCount = 0;
+            // 3. pref에 최근 메시지 시간 저장할 것
+            PrefsObject.setTotalLastMsgCreatedAt(
+                chatMessage.createdAt.toIso8601String());
+
+            // UI: 현재 위치가 해당방이면 ui 수정(chatdetail)
+            if (chatMessage.roomId == chatViewModel.insideRoomId) {
+              if (chatViewModel.fromFriendProfile) {
+                chatDetailFromFriendProfileViewModel
+                    .addChatMessage(chatMessage);
               } else {
-                chatRoom.unreadCount = unreadCount + 1;
+                chatDetailViewModel.addChatMessage(chatMessage);
               }
+            }
+            // ** UI(chatsScreen) 변경
+            int index = chatViewModel.getExistRoomIndex(chatMessage.roomId!);
+            // ChatRoom chatRoom = chatViewModel.chatRooms[index];
+            if (index == 0) {
+              // 최상단 : 업데이트만 => lastmsg, unreadcount, lastMsgCreatedAt
+              ChatRoom chatRoom = chatViewModel.chatRooms[index];
               chatRoom.lastMessage = chatMessage.text;
               chatRoom.lastMsgCreatedAt = chatMessage.createdAt;
-              chatViewModel.insertItem(chatRoom);
-            } else if (index == -1) {
-              ///한번 나간 방에서 다시 메세지가 올 때.
-              ///이 때는 chatViewModel.chatRooms에는 없지만 chatViewModel.exitedChatRooms 에는 있다.
-              ///여기서 꺼내와야함. 그래야 구독끊는 함수가 그대로 저장되어있음
+              // 해당 방 안에 아닐 때만 unreadcount 변경
+              if (chatMessage.roomId != chatViewModel.insideRoomId) {
+                chatRoom.unreadCount = chatRoom.unreadCount + 1;
+              }
+              chatViewModel.replaceItem(chatRoom, index);
+            } else {
+              // 최상단 아니면 => 1. 기존 방: 방 지웠다가 새롭게 방 추가  2. 완전 처음 방: 새롭게 방만 추가
 
-              for (ChatRoom exitedChatRoom in chatViewModel.exitedChatRooms) {
-                if (exitedChatRoom.roomId == res["roomId"]) {
-                  ChatRoom chatRoom = exitedChatRoom;
-                  //1.바뀌어야 하는 정보 변경
-                  chatRoom.lastMessage = chatMessage.text;
-                  chatRoom.lastMsgCreatedAt = chatMessage.createdAt;
-                  chatRoom.unreadCount = 1;
-                  //2. chatsScreen 수정
-                  chatViewModel.insertItem(chatRoom);
-                  //나갔다가 다시 들어가는거니까 더이상 나간방에 스페어로 저장해둘 필요없음. 오히려 계속 놔두면 같은방을 또 나갔을때 여기 계속 쌓이게 됨. 그래서 지워준다.
-                  //아직 테스트는 안해봄(12.15)
-                  chatViewModel.exitedChatRooms.remove(exitedChatRoom);
-                  break;
+              if (index >= 0) {
+                // 기존에 있는 방: unreadCount, lastMsg, lastMsgCreatedAt 변경
+                // 현재 그 방 안이라면 0으로 값 변경
+                // 아니면 삭제(removeItem)하면서 unreadcount 반환 (그 값으로 변경)
+                ChatRoom chatRoom = chatViewModel.chatRooms[index];
+                int unreadCount =
+                    chatViewModel.removeItem(index, chatRoom.roomId);
+                if (chatMessage.roomId == chatViewModel.insideRoomId) {
+                  chatRoom.unreadCount = 0;
+                } else {
+                  chatRoom.unreadCount = unreadCount + 1;
+                }
+                chatRoom.lastMessage = chatMessage.text;
+                chatRoom.lastMsgCreatedAt = chatMessage.createdAt;
+                chatViewModel.insertItem(chatRoom);
+              } else if (index == -1) {
+                ///한번 나간 방에서 다시 메세지가 올 때.
+                ///이 때는 chatViewModel.chatRooms에는 없지만 chatViewModel.exitedChatRooms 에는 있다.
+                ///여기서 꺼내와야함. 그래야 구독끊는 함수가 그대로 저장되어있음
+
+                for (ChatRoom exitedChatRoom in chatViewModel.exitedChatRooms) {
+                  if (exitedChatRoom.roomId == res["roomId"]) {
+                    ChatRoom chatRoom = exitedChatRoom;
+                    //1.바뀌어야 하는 정보 변경
+                    chatRoom.lastMessage = chatMessage.text;
+                    chatRoom.lastMsgCreatedAt = chatMessage.createdAt;
+                    chatRoom.unreadCount = 1;
+                    //2. chatsScreen 수정
+                    chatViewModel.insertItem(chatRoom);
+                    //나갔다가 다시 들어가는거니까 더이상 나간방에 스페어로 저장해둘 필요없음. 오히려 계속 놔두면 같은방을 또 나갔을때 여기 계속 쌓이게 됨. 그래서 지워준다.
+                    //아직 테스트는 안해봄(12.15)
+                    chatViewModel.exitedChatRooms.remove(exitedChatRoom);
+                    break;
+                  }
                 }
               }
             }
+            chatViewModel.getTotalUnreadCount();
           }
-          chatViewModel.getTotalUnreadCount();
         } else if (res["type"] == "enter") {
           /***** ENTER ******/
           /* 1. 메세지 저장 */
@@ -305,31 +312,39 @@ class StompObject {
           _changeMemberInfo(chatMemberInfo);
         } else {
           /***** CREATE ******/
-          /* 1. 채팅방정보 저장 및 구독 */
-          ChatRoom chatRoom = ChatRoom(
-              roomId: res["roomInfo"]["roomId"],
-              title: res["roomInfo"]["title"],
-              type: res["roomInfo"]["type"],
-              lastMessage: "대화를 시작해보세요!",
-              lastMsgCreatedAt:
-                  DateTime(2021, 5, 5), //TODO: DateTime.now()로 바꾸기
-              imageUrl: res["roomInfo"]["imageUrl"],
-              unreadCount: 0);
-          _chatService.insertChatRoom(chatRoom);
-          chatRoom.unsubscribeFn = subscribeChatRoom(chatRoom.roomId);
+          bool isBlocked = await PrefsObject.getBlockedUserByLoginId(
+              res["requestedMember"]["loginId"]);
+          if (isBlocked) {
+            print('차단한 사람한테서 첫 메세지 요청이 왔고, 스톰으로 구독이 됐고 메세지를 받아왔는데 ui에 띄워주지 않음');
+            return;
+          } else {
+            /* 1. 채팅방정보 저장 및 구독 */
+            ChatRoom chatRoom = ChatRoom(
+                roomId: res["roomInfo"]["roomId"],
+                title: res["roomInfo"]["title"],
+                type: res["roomInfo"]["type"],
+                lastMessage: "대화를 시작해보세요!",
+                lastMsgCreatedAt:
+                    DateTime(2021, 5, 5), //TODO: DateTime.now()로 바꾸기
+                imageUrl: res["roomInfo"]["imageUrl"],
+                unreadCount: 0);
+            _chatService.insertChatRoom(chatRoom);
+            chatRoom.unsubscribeFn = subscribeChatRoom(chatRoom.roomId);
 
-          /* 2. 멤버 저장 */
-          res["memberInfos"].forEach((memberInfo) {
-            ChatMemberInfo chatMemberInfo = ChatMemberInfo.fromJson(memberInfo);
-            ChatRoomMemberInfo chatRoomMemberInfo = ChatRoomMemberInfo(
-                roomId: chatRoom.roomId, loginId: memberInfo["loginId"]);
-            _chatService.insertOrUpdateMemberInfoOne(chatMemberInfo);
-            _chatService.insertChatRoomMemberInfoOne(chatRoomMemberInfo);
-          });
+            /* 2. 멤버 저장 */
+            res["memberInfos"].forEach((memberInfo) {
+              ChatMemberInfo chatMemberInfo =
+                  ChatMemberInfo.fromJson(memberInfo);
+              ChatRoomMemberInfo chatRoomMemberInfo = ChatRoomMemberInfo(
+                  roomId: chatRoom.roomId, loginId: memberInfo["loginId"]);
+              _chatService.insertOrUpdateMemberInfoOne(chatMemberInfo);
+              _chatService.insertChatRoomMemberInfoOne(chatRoomMemberInfo);
+            });
 
-          // /* 3. chatsScreen 수정 */
-          chatViewModel.insertItem(chatRoom);
-          chatViewModel.getTotalUnreadCount();
+            // /* 3. chatsScreen 수정 */
+            chatViewModel.insertItem(chatRoom);
+            chatViewModel.getTotalUnreadCount();
+          }
         }
       },
     );
