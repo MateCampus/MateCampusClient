bfee96aee74a13fe0e1e67616dd7c499b20fd8d1
S-YUN <candy9won@naver.com>
2023-04-16 14:34:52 +0900

    서버 주소 정리: 로컬 및 실서버 주소만 남김

--- lib/src/business_logic/utils/constants.dart
+++ lib/src/business_logic/utils/constants.dart
@@ -5,14 +5,12 @@ import 'package:zamongcampus/src/config/size_config.dart';
 //가로패딩 값.(사용중->옮길예정)
 double kHorizontalPadding = getProportionateScreenWidth(20);
 
-// server
-// final devServer = Platform.isAndroid
-//     ? "https://52d6-222-235-213-23.jp.ngrok.io"
-//     : "https://52d6-222-235-213-23.jp.ngrok.io";
-
+/// server
+// local
 // final devServer =
 //     Platform.isAndroid ? "http://172.30.1.22:8080" : "http://172.30.1.22:8080";
 
+// 실서버
 final devServer = Platform.isAndroid
     ? "https://matecampus.duckdns.org:8080"
     : "https://matecampus.duckdns.org:8080";
